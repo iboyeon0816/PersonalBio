864937dda015e6e9e98ff86328807e940148c1b5
김종연 <bellopenerr@gmail.com>
2024-12-26 14:42:42 +0900

    chore: viteConfig baseUrl 변경

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
+  base: /PersonalBio/,
   plugins: [
     obfuscatorPlugin({
       exclude: [/node_modules/, "src/repository/key.dev.js"],
